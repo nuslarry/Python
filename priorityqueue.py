a2847784bc0d7605581d7645aeef6728ed2ae810
Cheng Liang Yu <chengly3@uci.edu>
2019-04-21 16:53:18 -0700

    Update priorityqueue.py

--- priorityqueue.py
+++ priorityqueue.py
@@ -3,6 +3,10 @@ a=[3,100,99,5,6,9]
 heapq.heapify(a)
 a.remove(3)
 heapq.heapify(a)
-a.remove(5)
+a.remove(5) #自個remove要再heapify
 heapq.heapify(a)
 print(a[0])
+heapq.heappop(a)
+print(a)
+heapq.heappush(a,4)
+print(a)
